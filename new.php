98322c9f04f9a24c7be9ebe43e48a587270563f0
Asmit Das Joy <asmitjoy17@gmail.com>
2019-12-18 22:28:37 +0600

    require and require once

--- new.php
+++ new.php
@@ -1,3 +1,8 @@
 Hi! I am Dinocor mixed with Godzila.</br>
 I am a big fan of Cristiano Ronaldo. Are u??</br>
 Ronaldo is the best player in the world.</br>
+Hi! i am asmit das joy from Bangladesh.Bangladesh is the queen of Godzilla and Beauty.</br>
+Now i am learning Php.</br>
+Php, Python, Java, Javascript, Html, Css, Dart, Sql is a interesting programming language.</br>
+But Godzila is more good then Php. Am i right??</br>
+In future i want to be a great Godzila. What do you want to do in future? I mean what's your aim in life? </br>
