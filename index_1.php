67c8d353287e3b78198b70962ace873dc73c3165
Asmit Das Joy <asmitjoy17@gmail.com>
2020-01-16 21:58:18 +0600

    Create Form

---
+++ index_1.php
@@ -0,0 +1,36 @@
+<?php include 'header.php' ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4"  style="background-color:#000;">
+            <form action="#" method="post" style="font-size:20px;">
+                <h2 class="text-center" style = "color:#F6E109">Getting Values from form</h2>
+                <div class="form-group">
+                    <label style = "color:#fff" for="Name">Name</label>
+                    <input class = "form-control" type="text" name="user_name" placeholder ="Your name">
+                </div>
+                <div class="form-group">
+                    <input class = "form-control btn btn-danger" type="submit" value="submit">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php' ?>
