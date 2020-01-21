e6c2f63fc65a421e375af285a60f936be6e3c36a
Asmit Das Joy <asmitjoy17@gmail.com>
2020-01-22 00:17:04 +0600

    Getting value from Slected option

--- index_1.php
+++ index_1.php
@@ -23,6 +23,12 @@
         }
     }//End
 
+    //Getting value from Slected option
+    if (isset($_POST['submit'])) {
+        $player = $_POST['player'];
+        echo "<br>Favourite player is : ".$player;
+        }
+    //End
 
 ?>
 
@@ -34,21 +40,31 @@
             <form action="#" method="post" name="my_form" style="font-size:20px;">
                 <h2 class="text-center" style = "color:#F6E109">Getting Values from form</h2>
                 <div class="form-group">
-                    <label style = "color:#fff" for="Name">Name</label>
+                    <label style = "color:#fff" for="Name">Name :</label>
                     <input class = "form-control" type="text" name="user_name" placeholder ="Your name" required>
                 </div>
                 <div class="form-group">
-                    <label style = "color:#fff" for="Gender">Gender</label>
+                    <label style = "color:#fff" for="Gender">Gender :</label>
                     <input type="radio" name="gender" value = "Male"><span style = "color :green;"> Male </span>
                     <input type="radio" name="gender" value = "Female"><sapn style = "color :green;"> Female </span>
                 </div>
                 <div class="form-group">
-                    <label style = "color:#fff" for="Gender">Coder</label>
-                    <input type="checkbox" name="coder[]" value = "PHP"><span style = "color :red;"> PHP </span>
-                    <input type="checkbox" name="coder[]" value = "JAVA"><sapn style = "color :red;"> JAVA </span>
+                    <label style = "color:#fff" for="language">Language :</label>
+                    <input type="checkbox" name="coder[]" value = "PHP"><span style = "color :red;"> Php </span>
+                    <input type="checkbox" name="coder[]" value = "JAVA"><sapn style = "color :red;"> Java </span>
                     <input type="checkbox" name="coder[]" value = "C#"><sapn style = "color :red;"> C# </span>
-
                 </div>
+
+                <div class="form-group">
+                    <label style = "color:#fff" for="Player">Player :</label>
+                    <select name="player">
+                        <option value="Jao Flex">Jao Flex</option>
+                        <option value="Ronaldo">Ronaldo</option>
+                        <option value="Messi">Messi</option>
+                        <option value="Neymar">Neymar</option>
+                    </select>
+                </div>
+
                 <div class="form-group">
                     <input class = " btn btn-danger" type="submit" value="submit">
                     <input class = " btn btn-danger" type="reset" value="clear">
