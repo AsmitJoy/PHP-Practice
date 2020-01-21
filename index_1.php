d81db12820d0379b2cab5689a1ffa39c7de78c11
Asmit Das Joy <asmitjoy17@gmail.com>
2020-01-21 23:09:35 +0600

    Get value from a Text Box

--- index_1.php
+++ index_1.php
@@ -1,16 +1,29 @@
 <?php include 'header.php' ?>
 
+<!-- Php code -->
+<?php
+    if (isset($_POST['user_name'])) {
+        $name = $_POST['user_name'];
+        echo"Username is : ".$name;
+    }
+?>
+
+Get value from a Text Box
+
+<!-- My form -->
 <div class="container">
     <div class="row">
         <div class="col-md-4 col-md-offset-4"  style="background-color:#000;">
-            <form action="#" method="post" style="font-size:20px;">
+            <form action="#" method="post" name="my_form" style="font-size:20px;">
                 <h2 class="text-center" style = "color:#F6E109">Getting Values from form</h2>
                 <div class="form-group">
                     <label style = "color:#fff" for="Name">Name</label>
-                    <input class = "form-control" type="text" name="user_name" placeholder ="Your name">
+                    <input class = "form-control" type="text" name="user_name" placeholder ="Your name" required>
                 </div>
                 <div class="form-group">
-                    <input class = "form-control btn btn-danger" type="submit" value="submit">
+                    <input class = " btn btn-danger" type="submit" value="submit">
+                    <input class = " btn btn-danger" type="reset" value="clear">
+
                 </div>
             </form>
         </div>
